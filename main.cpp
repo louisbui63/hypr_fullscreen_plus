701ebb6ebf0d896e7d6e0632c06929f1364e5d92
Louis Buisson <louisbui@vivaldi.net>
2024-01-15 13:19:30 +0100

    fix behavior when there is already a fullscreen window

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include "hyprland/src/Compositor.hpp"
 #include "hyprland/src/Window.hpp"
 #include "hyprland/src/plugins/PluginAPI.hpp"
 #include <cstdint>
@@ -11,19 +12,31 @@ struct {
   bool pinned;
   Vector2D size;
   Vector2D position;
+  bool was_already_fs;
+  CWindow *old_fs;
 
 } typedef Status;
 
 std::unordered_map<uintptr_t, Status> PINNED_FULLSCREEN{};
 
-typedef void (*origSetWindowFullscreen)(void *, CWindow *, bool, int8_t);
+typedef void (*origSetWindowFullscreen)(CCompositor *, CWindow *, bool, int8_t);
 
-void hkSetWindowFullscreen(void *thisptr, CWindow *pWindow, bool on,
+void hkSetWindowFullscreen(CCompositor *thisptr, CWindow *pWindow, bool on,
                            int8_t mode) {
+  const auto PWORKSPACE = thisptr->getWorkspaceByID(pWindow->m_iWorkspaceID);
   if (on) {
+    bool was_already_fs = false;
+    CWindow *old_fs = nullptr;
+    if (PWORKSPACE->m_bHasFullscreenWindow) {
+      CWindow *fs_w =
+          thisptr->getFullscreenWindowOnWorkspace(pWindow->m_iWorkspaceID);
+      thisptr->setWindowFullscreen(fs_w, false, FULLSCREEN_FULL);
+      was_already_fs = true;
+      old_fs = fs_w;
+    }
 
     Status st = {pWindow->m_bPinned, pWindow->m_vRealSize.vec(),
-                 pWindow->m_vRealPosition.vec()};
+                 pWindow->m_vRealPosition.vec(), was_already_fs, old_fs};
     pWindow->m_bPinned = false;
 
     PINNED_FULLSCREEN[(uintptr_t)pWindow] = st;
@@ -38,6 +51,9 @@ void hkSetWindowFullscreen(void *thisptr, CWindow *pWindow, bool on,
     if (pWindow->m_bIsFloating) {
       pWindow->m_vRealSize = u.size;
       pWindow->m_vRealPosition = u.position;
+    }
+    if (u.was_already_fs) {
+      thisptr->setWindowFullscreen(u.old_fs, true, FULLSCREEN_FULL);
     }
     PINNED_FULLSCREEN.erase((uintptr_t)pWindow);
   }
