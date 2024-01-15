db3ab38c33b5dde3db197c8180dd90bc56610705
Louis Buisson <louisbui@vivaldi.net>
2024-01-16 00:47:15 +0100

    still another bug. don't know what it was, and most of the code is probably useless, but it's fixed

--- main.cpp
+++ main.cpp
@@ -23,13 +23,16 @@ typedef void (*origSetWindowFullscreen)(CCompositor *, CWindow *, bool, int8_t);
 
 void hkSetWindowFullscreen(CCompositor *thisptr, CWindow *pWindow, bool on,
                            int8_t mode) {
+
   const auto PWORKSPACE = thisptr->getWorkspaceByID(pWindow->m_iWorkspaceID);
-  if (on) {
+
+  if (on && pWindow->m_bIsFloating) {
     bool was_already_fs = false;
     CWindow *old_fs = nullptr;
     if (PWORKSPACE->m_bHasFullscreenWindow) {
       CWindow *fs_w =
           thisptr->getFullscreenWindowOnWorkspace(pWindow->m_iWorkspaceID);
+
       thisptr->setWindowFullscreen(fs_w, false, FULLSCREEN_FULL);
       was_already_fs = true;
       old_fs = fs_w;
@@ -37,25 +40,68 @@ void hkSetWindowFullscreen(CCompositor *thisptr, CWindow *pWindow, bool on,
 
     Status st = {pWindow->m_bPinned, pWindow->m_vRealSize.vec(),
                  pWindow->m_vRealPosition.vec(), was_already_fs, old_fs};
-    pWindow->m_bPinned = false;
 
     PINNED_FULLSCREEN[(uintptr_t)pWindow] = st;
-    pWindow->m_bPinned = false;
+    if (pWindow->m_bPinned) {
+      pWindow->m_bPinned = false;
+      pWindow->m_iWorkspaceID =
+          thisptr->getMonitorFromID(pWindow->m_iMonitorID)->activeWorkspace;
+
+      pWindow->updateDynamicRules();
+      thisptr->updateWindowAnimatedDecorationValues(pWindow);
+
+      const auto PWORKSPACE =
+          thisptr->getWorkspaceByID(pWindow->m_iWorkspaceID);
+
+      PWORKSPACE->m_pLastFocusedWindow = thisptr->vectorToWindowTiled(
+          g_pInputManager->getMouseCoordsInternal());
+    }
+    pWindow->m_bIsFloating = !pWindow->m_bIsFloating;
+    pWindow->updateDynamicRules();
+    g_pLayoutManager->getCurrentLayout()->changeWindowFloatingMode(pWindow);
   }
 
   (*(origSetWindowFullscreen)g_pSetWindowFullscreenHook->m_pOriginal)(
       thisptr, pWindow, on, mode);
+
   if (!on && PINNED_FULLSCREEN.contains((uintptr_t)pWindow)) {
+    pWindow->m_bIsFloating = !pWindow->m_bIsFloating;
+    pWindow->updateDynamicRules();
+    g_pLayoutManager->getCurrentLayout()->changeWindowFloatingMode(pWindow);
     auto u = PINNED_FULLSCREEN[(uintptr_t)pWindow];
-    pWindow->m_bPinned = u.pinned;
     if (pWindow->m_bIsFloating) {
-      pWindow->m_vRealSize = u.size;
-      pWindow->m_vRealPosition = u.position;
+      // pWindow->m_vRealSize = u.size;
+      g_pLayoutManager->getCurrentLayout()->resizeActiveWindow(
+          u.size - pWindow->m_vRealSize.goalv(), CORNER_NONE, pWindow);
+      if (pWindow->m_vRealSize.goalv().x > 1 &&
+          pWindow->m_vRealSize.goalv().y > 1)
+        pWindow->setHidden(false);
+      // pWindow->m_vRealPosition = u.position;
+      g_pLayoutManager->getCurrentLayout()->moveActiveWindow(
+          u.position - pWindow->m_vRealPosition.goalv(), pWindow);
     }
+
+    if (pWindow->m_bIsFloating && u.pinned) {
+      pWindow->m_bPinned = true;
+      pWindow->m_iWorkspaceID =
+          thisptr->getMonitorFromID(pWindow->m_iMonitorID)->activeWorkspace;
+
+      pWindow->updateDynamicRules();
+      thisptr->updateWindowAnimatedDecorationValues(pWindow);
+
+      const auto PWORKSPACE =
+          thisptr->getWorkspaceByID(pWindow->m_iWorkspaceID);
+
+      PWORKSPACE->m_pLastFocusedWindow = thisptr->vectorToWindowTiled(
+          g_pInputManager->getMouseCoordsInternal());
+    }
+
     if (u.was_already_fs) {
       if (thisptr->windowExists(u.old_fs))
-        thisptr->setWindowFullscreen(u.old_fs, true, FULLSCREEN_FULL);
+        (*(origSetWindowFullscreen)g_pSetWindowFullscreenHook->m_pOriginal)(
+            thisptr, u.old_fs, true, FULLSCREEN_FULL);
     }
+
     PINNED_FULLSCREEN.erase((uintptr_t)pWindow);
   }
 }
