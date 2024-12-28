39f592bd67b1c03390d24e59ecd5605ac58bbe64
louisbui63 <louisbui@vivaldi.net>
2024-12-29 00:25:30 +0100

    fix weird behavior with current allow_pin_fullscreen implementation + bump internal version

--- main.cpp
+++ main.cpp
@@ -1,23 +1,10 @@
 #include "hyprland/src/Compositor.hpp"
 #include "hyprland/src/desktop/Window.hpp"
 #include "hyprland/src/plugins/PluginAPI.hpp"
-#include <unordered_map>
 
 inline HANDLE PHANDLE = nullptr;
 
 inline CFunctionHook *g_pSetWindowFullscreenHook = nullptr;
-
-struct {
-  bool pinned;
-  Vector2D size;
-  Vector2D position;
-  bool was_already_fs;
-  PHLWINDOW old_fs;
-  SFullscreenState old_fs_state;
-
-} typedef Status;
-
-std::unordered_map<PHLWINDOW, Status> PINNED_FULLSCREEN{};
 
 typedef void (*origSetWindowFullscreen)(CCompositor *, PHLWINDOW,
                                         SFullscreenState);
@@ -25,90 +12,12 @@ typedef void (*origSetWindowFullscreen)(CCompositor *, PHLWINDOW,
 void hkSetWindowFullscreen(CCompositor *thisptr, PHLWINDOW pWindow,
                            SFullscreenState state) {
 
-  bool on = state.client & FSMODE_FULLSCREEN;
-
-  const auto PWORKSPACE = thisptr->getWorkspaceByID(pWindow->workspaceID());
-
-  if (on && pWindow->m_bIsFloating) {
-    bool was_already_fs = false;
-    PHLWINDOW old_fs = nullptr;
-    if (PWORKSPACE->m_bHasFullscreenWindow) {
-      PHLWINDOW fs_w = PWORKSPACE->getFullscreenWindow();
-
-      thisptr->setWindowFullscreenState(
-          fs_w,
-          SFullscreenState{.internal = FSMODE_NONE, .client = FSMODE_NONE});
-      was_already_fs = true;
-      old_fs = fs_w;
-    }
-
-    Status st = {pWindow->m_bPinned, pWindow->m_vRealSize.value(),
-                 pWindow->m_vRealPosition.value(), was_already_fs, old_fs};
-
-    PINNED_FULLSCREEN[pWindow] = st;
-    if (pWindow->m_bPinned) {
-      pWindow->m_bPinned = false;
-      // pWindow->workspaceID() =
-      //     thisptr->getMonitorFromID(pWindow->m_iMonitorID)->activeWorkspace;
-      pWindow->moveToWorkspace(
-          thisptr->getMonitorFromID(pWindow->m_pMonitor)->activeWorkspace);
-
-      pWindow->updateDynamicRules();
-      thisptr->updateWindowAnimatedDecorationValues(pWindow);
-
-      const auto PWORKSPACE = thisptr->getWorkspaceByID(pWindow->workspaceID());
-
-      PWORKSPACE->m_pLastFocusedWindow = thisptr->vectorToWindowUnified(
-          g_pInputManager->getMouseCoordsInternal(), USE_PROP_TILED);
-    }
-    pWindow->m_bIsFloating = !pWindow->m_bIsFloating;
-    pWindow->updateDynamicRules();
-    g_pLayoutManager->getCurrentLayout()->changeWindowFloatingMode(pWindow);
-  }
-
   (*(origSetWindowFullscreen)g_pSetWindowFullscreenHook->m_pOriginal)(
       thisptr, pWindow, state);
 
-  if (!on && PINNED_FULLSCREEN.contains(pWindow)) {
-    pWindow->m_bIsFloating = !pWindow->m_bIsFloating;
-    pWindow->updateDynamicRules();
-    g_pLayoutManager->getCurrentLayout()->changeWindowFloatingMode(pWindow);
-    auto u = PINNED_FULLSCREEN[pWindow];
-    if (pWindow->m_bIsFloating) {
-      // pWindow->m_vRealSize = u.size;
-      g_pLayoutManager->getCurrentLayout()->resizeActiveWindow(
-          u.size - pWindow->m_vRealSize.goal(), CORNER_NONE, pWindow);
-      if (pWindow->m_vRealSize.goal().x > 1 &&
-          pWindow->m_vRealSize.goal().y > 1)
-        pWindow->setHidden(false);
-      // pWindow->m_vRealPosition = u.position;
-      g_pLayoutManager->getCurrentLayout()->moveActiveWindow(
-          u.position - pWindow->m_vRealPosition.goal(), pWindow);
-    }
-
-    if (pWindow->m_bIsFloating && u.pinned) {
-      pWindow->m_bPinned = true;
-      // pWindow->workspaceID() =
-      //     thisptr->getMonitorFromID(pWindow->m_iMonitorID)->activeWorkspace;
-      pWindow->moveToWorkspace(
-          thisptr->getMonitorFromID(pWindow->m_pMonitor)->activeWorkspace);
-
-      pWindow->updateDynamicRules();
-      thisptr->updateWindowAnimatedDecorationValues(pWindow);
-
-      const auto PWORKSPACE = thisptr->getWorkspaceByID(pWindow->workspaceID());
-
-      PWORKSPACE->m_pLastFocusedWindow = thisptr->vectorToWindowUnified(
-          g_pInputManager->getMouseCoordsInternal(), USE_PROP_TILED);
-    }
-
-    if (u.was_already_fs) {
-      if (valid(u.old_fs))
-        (*(origSetWindowFullscreen)g_pSetWindowFullscreenHook->m_pOriginal)(
-            thisptr, u.old_fs, u.old_fs_state);
-    }
-
-    PINNED_FULLSCREEN.erase(pWindow);
+  if (pWindow->m_bPinFullscreened && state.internal == FSMODE_NONE) {
+    pWindow->m_bPinned = true;
+    pWindow->m_bPinFullscreened = false;
   }
 }
 
@@ -125,7 +34,7 @@ APICALL EXPORT PLUGIN_DESCRIPTION_INFO PLUGIN_INIT(HANDLE handle) {
   g_pSetWindowFullscreenHook->hook();
 
   return {"hypr_fullscreen_plus", "Makes fullscreen better", "louisbui63",
-          "0.0.1"};
+          "0.0.2"};
 }
 
 APICALL EXPORT void PLUGIN_EXIT() {}
