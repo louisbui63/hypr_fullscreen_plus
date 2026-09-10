7329bedb1e0aa825db4f1f5b40c7e4786e6ea0cd
louisbui63 <louisbui@vivaldi.net>
2026-09-10 20:01:07 +0200

    fix behavioral issues in latest version

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 // using namespace Desktop::View;
 using namespace Fullscreen;
 
-#ifdef DEBUG
+#ifdef __DEBUG
 #include <string>
 #endif
 #include <unordered_map>
@@ -34,13 +34,13 @@ void hkSetFullscreenMode(CFullscreenController *thisptr, PHLWINDOW pWindow,
                          std::optional<eFullscreenMode> client = std::nullopt,
                          std::optional<bool> layoutAware = std::nullopt) {
 
-#ifdef DEBUG
+#ifdef __DEBUG
   HyprlandAPI::addNotification(
       PHANDLE,
-      pWindow->m_title + ";" + std::to_string(pWindow->isFullscreen()) + "->" +
-          std::to_string(state.internal) + ";" + std::to_string(state.client) +
-          "~~" + std::to_string(pWindow->m_workspace->m_id) + "(" +
-          std::to_string(pWindow->m_workspace->m_hasFullscreenWindow) + ")",
+      pWindow->m_title + ";" + std::to_string(thisptr->isFullscreen(pWindow)) +
+          "->" + (internal ? std::to_string(*internal) : "none") + ";" +
+          (client ? std::to_string(*client) : "none") + "~~" +
+          std::to_string(pWindow->m_workspace->m_id),
       CHyprColor(1, 1, 1, 1), 5000);
 #endif
 
@@ -50,13 +50,44 @@ void hkSetFullscreenMode(CFullscreenController *thisptr, PHLWINDOW pWindow,
       (!client || modes.client == *client))
     return;
 
+  // SFullscreenMode org_modes = thisptr->getFullscreenModes(pWindow);
+
+  // (*(origSetFullscreenMode)g_setFullscreenModeHook->m_original)(
+  //     thisptr, pWindow, internal, client, layoutAware);
+
+  // (*(origSetFullscreenMode)g_setFullscreenModeHook->m_original)(
+  //     thisptr, pWindow, org_modes.internal, org_modes.client, layoutAware);
+
   eFullscreenMode target_internal = (internal) ? *internal : modes.internal;
   eFullscreenMode target_client = (client) ? *client : modes.client;
+  if (internal.has_value() && !client.has_value())
+    target_client = target_internal;
+  else
+    target_internal = target_client;
+
+  // #ifdef __DEBUG
+  //   HyprlandAPI::addNotification(
+  //       PHANDLE,
+  //       pWindow->m_title + ";" +
+  //           std::to_string(modes.internal == target_internal) + "<->" +
+  //           std::to_string(modes.client == target_client),
+  //       CHyprColor(1, 0, 1, 1), 5000);
+  //   HyprlandAPI::addNotification(PHANDLE,
+  //                                pWindow->m_title + ";" +
+  //                                    std::to_string(modes.internal) + "<->" +
+  //                                    std::to_string(modes.client),
+  //                                CHyprColor(1, 0, 0, 1), 5000);
+  //
+  // #endif
 
   if (/*pWindow->m_bPinned && */ !thisptr->isFullscreen(pWindow) &&
       target_internal != FSMODE_NONE) {
     auto w = pWindow->m_workspace;
     if (thisptr->hasFullscreen(w)) {
+#ifdef __DEBUG
+      HyprlandAPI::addNotification(PHANDLE, pWindow->m_title + " store",
+                                   CHyprColor(0, 0, 1, 1), 5000);
+#endif
       PHLWINDOW wfs = thisptr->getFullscreenWindow(w);
       previous_fs[pWindow] = {wfs, thisptr->getFullscreenModes(wfs)};
     }
@@ -64,13 +95,16 @@ void hkSetFullscreenMode(CFullscreenController *thisptr, PHLWINDOW pWindow,
 
   (*(origSetFullscreenMode)g_setFullscreenModeHook->m_original)(
       thisptr, pWindow, internal, client, layoutAware);
-
   if (pWindow->m_pinFullscreened && target_internal == FSMODE_NONE) {
     pWindow->m_pinned = true;
     pWindow->m_pinFullscreened = false;
   }
 
   if (target_internal == FSMODE_NONE && previous_fs.contains(pWindow)) {
+#ifdef __DEBUG
+    HyprlandAPI::addNotification(PHANDLE, pWindow->m_title + " get",
+                                 CHyprColor(0, 0, 1, 1), 5000);
+#endif
     Status old_fs = previous_fs[pWindow];
     if (valid(old_fs.window))
       (*(origSetFullscreenMode)g_setFullscreenModeHook->m_original)(
@@ -86,7 +120,6 @@ inline CFunctionHook *g_pCloseWindowHook = nullptr;
 typedef void (*origCloseWindow)(CCompositor *, PHLWINDOW);
 
 void hkCloseWindow(CCompositor *thisptr, PHLWINDOW pWindow) {
-  HyprlandAPI::addNotification(PHANDLE, "", CHyprColor(1, 1, 1, 1), 0);
   if (Fullscreen::controller()->isFullscreen(pWindow) &&
       previous_fs.contains(pWindow)) {
     Status old_fs = previous_fs[pWindow];
